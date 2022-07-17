b42818d561dbec7b81626de0e6f92097f629d5c7
Klaasjan Maas <klaasjan@floki.io>
2022-07-17 18:56:02 +0200

    added transparent background

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -13,6 +13,7 @@ lvim.log.level = "warn"
 -- lvim.format_on_save = true
 lvim.format_on_save = false
 lvim.colorscheme = "onedarker"
+lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 

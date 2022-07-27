573d3c615db96b1780390ac079dfea2432da9f85
Klaasjan Maas <klaasjan@floki.io>
2022-07-27 23:59:43 +0200

    use absolute path and local timezone

--- sway/.config/sway/wallpaper/dynamic_wallpaper.sh
+++ sway/.config/sway/wallpaper/dynamic_wallpaper.sh
@@ -5,7 +5,7 @@ bash ./convert_to_time.sh "$1"
 cd "$HOME/Pictures/Walpapers/$1/" || return
 while true; do
   PID=$(pidof swaybg)
-    swaybg -i ./$(date -u -d @$((($(date -u +%s) / 600) * 600)) "+%Hh%M").png -m fill &
+    swaybg -i ./$(date -d @$((($(date +%s) / 600) * 600)) "+%Hh%M").png -m fill &
     # swaybg -i ./16h30.png -m fill &
     sleep 1
     kill "$PID"

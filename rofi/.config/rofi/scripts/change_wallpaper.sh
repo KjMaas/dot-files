ebc9e700e4f7fb91cf8f3d620ccdb47fce865173
Klaasjan Maas <klaasjan.maas@outlook.com>
2022-11-26 15:49:32 +0100

    Make the script more robust

--- rofi/.config/rofi/scripts/change_wallpaper.sh
+++ rofi/.config/rofi/scripts/change_wallpaper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/run/current-system/sw/bin/bash
 
 
 wallpaper=$(find "$HOME"/Pictures/Wallpapers/* -maxdepth 0 | rofi -dmenu -p "Choose Dynamic Wallpaper" | cut -d "/" -f 6);
@@ -6,7 +6,7 @@ wallpaper=$(find "$HOME"/Pictures/Wallpapers/* -maxdepth 0 | rofi -dmenu -p "Cho
 # kill all the running 'dynamic_wallpaper.sh' scripts and the child processes
 ps x -o  "%p %r %c" | grep dynamic_wall | awk -F" " '{print $2}' | xargs pkill -g
 
-cd "$HOME/dot-files/sway/.config/sway/wallpaper/" || exit
+cd "$HOME/dot-files/services/wallpaper/" || exit
 
 echo "setting wallpaper to: $wallpaper";
 ./dynamic_wallpaper.sh "$wallpaper"

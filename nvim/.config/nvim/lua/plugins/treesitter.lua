4c4cc3b200ad05f87b5a19d58f6d5658a0075ecc
Klaasjan Maas <klaasjan.maas@outlook.com>
2022-12-07 11:51:12 +0100

    add incremental selection with TS

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -96,6 +96,16 @@ ts.setup {
 
   indent = {
     enable = true,
-  }
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 
 }
